7ad33127bf95b29d2ce3332d3231ed754a31c630
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-06-27 20:25:09 -0300

    Funcional

--- Foreach.java
+++ Foreach.java
@@ -7,14 +7,24 @@ public class Foreach {
                 .asList("Ana", "Bia", "Lia", "Gui");
 
         System.out.println("Forma tradicional...");
-        for (String nome: aprovados) {
-            System.out.println(nome );
+        for (String nome : aprovados) {
+            System.out.println(nome);
         }
 
         System.out.println("\nLambda #1...");
         aprovados.forEach(nome -> System.out.println(nome + "!!"));
 
-        System.out.println("\nMethod Reference...");
+        System.out.println("\nMethod Reference #1...");
         aprovados.forEach(System.out::println);
+
+        System.out.println("\nLambda #2...");
+        aprovados.forEach(nome -> meuImprimir(nome));
+
+        System.out.println("\nMethod Reference #2...");
+        aprovados.forEach(Foreach::meuImprimir);
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println("Oi! Meu nome é " + nome);
     }
 }
